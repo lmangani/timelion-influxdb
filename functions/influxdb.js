fb69f4e996f7a095f3b41d9b6b4a2feae81bdc90
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-07-22 22:32:14 +0200

    fix more glitches

--- functions/influxdb.js
+++ functions/influxdb.js
@@ -1,8 +1,8 @@
-var alter = require('../../../src/core_plugins/timelion/server/lib/alter.js');
-var Datasource = require('../../../src/core_plugins/timelion/server/lib/classes/datasource');
-var fetch = require('node-fetch');
-fetch.Promise = require('bluebird');
-var _ = require('lodash');
+var alter = require('../../../src/core_plugins/timelion/server/lib/alter.js')
+var Datasource = require('../../../src/core_plugins/timelion/server/lib/classes/datasource')
+var fetch = require('node-fetch')
+fetch.Promise = require('bluebird')
+var _ = require('lodash')
 
 /*
   This timelion plugin can pull data from you InfluxDB deployment.
@@ -38,36 +38,36 @@ module.exports = new Datasource('influxdb', {
   ],
   help: 'Pull data from InfluxDB.',
 
-  fn: function influxdbFn(args, tlConfig) {
+  fn: function influxdbFn (args, tlConfig) {
     var config = _.defaults(args.byName, {
       deviceId: 1,
       chartId: 1000,
       label: 'Data from InfluxDB'
-    });
+    })
 
     /*
       For details of the URL, see the SL API Documentation.
       beginstamp and endstamp are both required, and need to be in Seconds
       since Epoch (Kibana provides them in milliseconds since Epoch)
     */
-    var beginTime = Math.floor(tlConfig.time.from / 1000);
-    var endTime = Math.floor(tlConfig.time.to / 1000);
-    var username = tlConfig.settings['timelion:influxdb.username'];
-    var password = tlConfig.settings['timelion:influxdb.password'];
+    var beginTime = Math.floor(tlConfig.time.from / 1000)
+    var endTime = Math.floor(tlConfig.time.to / 1000)
+    var username = tlConfig.settings['timelion:influxdb.username']
+    var password = tlConfig.settings['timelion:influxdb.password']
     var sl_hostname = tlConfig.settings['timelion:influxdb.hostname']
-    var sl_hostname = tlConfig.settings['timelion:influxdb.port'] || 8086
+    var sl_port = tlConfig.settings['timelion:influxdb.port'] || 8086
 
-    var URL = 'http://' + sl_hostname + ':' + sl_port + '/query';
+    var URL = 'http://' + sl_hostname + ':' + sl_port + '/query'
     var DATA = {
-	q: 'SELECT * from ' + config.metric + ' WHERE time > ' +begintime+ ' AND time < ' +endTime+ ',
-	db: config.db,
-	epoch: 's'
-    };
-    if(config.where) DATA.q += ' AND ' + config.where;
+      q: 'SELECT * from ' + config.metric + ' WHERE time > ' + beginTime + ' AND time < ' + endTime,
+      db: config.db,
+      epoch: 's'
+    }
+    if (config.where) DATA.q += ' AND ' + config.where
 
     if (!sl_hostname) {
       throw new Error('influxdb plugin: hostname, username and password must be configured. ' +
-        'Edit the file kibana/src/core_plugins/timelion/timelion.json. ');
+        'Edit the file kibana/src/core_plugins/timelion/timelion.json. ')
     }
 
     var urlOptions = {
@@ -80,12 +80,11 @@ module.exports = new Datasource('influxdb', {
         'x-em7-guid-paths': 1,
         'Accept-Encoding': 'gzip, deflate'
       }
-
-    if(username && password) {
-      var authString = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-      urlOptions.headers['Authorization'] = authString;
     }
 
+    if (username && password) {
+      var authString = 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
+      urlOptions.headers['Authorization'] = authString
     };
 
     /*
@@ -96,17 +95,16 @@ module.exports = new Datasource('influxdb', {
     /* */
 
     return fetch(URL, urlOptions).then(function (resp) {
-      return resp.json();
+      return resp.json()
     }).then(function (resp) {
-
       if (resp.errors) {
         throw new Error('Error connecting to influxdb: ' +
-          resp.errors[0].errorcode + ' ' + resp.errors[0].message);
+          resp.errors[0].errorcode + ' ' + resp.errors[0].message)
       }
       // SL supplies secs since epoch. Kibana wants ms since epoch
       var data = _.map(resp.resuts.series[0].values[0], function (timestamp, value) {
-        return [(timestamp * 1000), value ];
-      });
+        return [(timestamp * 1000), value ]
+      })
       return {
         type: 'seriesList',
         list: [{
@@ -115,9 +113,9 @@ module.exports = new Datasource('influxdb', {
           fit: 'nearest',
           label: config.label
         }]
-      };
+      }
     }).catch(function (e) {
-      throw e;
-    });
+      throw e
+    })
   }
-});
+})
